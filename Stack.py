675361f42755ea04764e00f3f6f6e0caf0943cfd
Berra Perek <73835553+berraperek@users.noreply.github.com>
2023-01-23 17:16:19 +0300

    Update Stack.py

--- Stack.py
+++ Stack.py
@@ -1,3 +1,15 @@
+"""
+You need to implement your stack as a class with a member variable(a container type, i.e list) and member functions. You
+will initialize the stack as:
+myStack = Stack()
+Your Stack class will have the following member functions:
+ • init (): A function that initializes the stack object.
+ • push(element): inserts a particular element into the stack.
+ • pop(): removes element from the top of the stack and returns the element.
+ • top(): returns the element that is at the top of the stack without removing the element.
+ • isempty(): return whether the stack is empty or not. Returns True when the stack is empty, False otherwise.
+ """
+
 class Stack:
     def __init__(self):
         self._data = []
