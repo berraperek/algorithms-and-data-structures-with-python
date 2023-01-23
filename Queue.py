386bcfd79a96bce0200a7b2f8bfb7b3ee1a9afa2
Berra Perek <73835553+berraperek@users.noreply.github.com>
2023-01-23 17:17:59 +0300

    Update Queue.py

--- Queue.py
+++ Queue.py
@@ -1,3 +1,16 @@
+"""
+You need to implement your queue as a class with a member variable(a container type, i.e list) and member functions. You
+will initialize the queue as:
+myQueue = Queue() 
+Your Queue class will have the following member functions:
+ • init (): A function that initializes the queue object.
+ • enqueue(element): inserts a particular element into the stack.
+ • dequeue(): removes element from the front of the queue and returns the element.
+ • front(): returns the element that is at the front of the queue without removing the element.
+ • isempty(): return whether the queue is empty or not. Returns True when the queue is empty, False otherwise.
+ 
+ """
+
 class Queue:
     def __init__(self):
         self._data = []
