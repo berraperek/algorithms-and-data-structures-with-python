126fb7c9a0f0a3186f3d5b5928d53de4ecac069f
Berra Perek <73835553+berraperek@users.noreply.github.com>
2023-01-23 17:19:46 +0300

    Add files via upload

---
+++ sieve_of_eratosthenes.py
@@ -0,0 +1,33 @@
+from Queue import Queue
+
+
+def sieve_of_eratosthenes(n):
+    numbers = Queue()
+
+    for i in range(2, n + 1):
+        numbers.enqueue(i)
+
+    result = Queue()
+
+    while True:
+        value = numbers.dequeue()
+        result.enqueue(value)
+        queue_length = len(numbers)
+        i = 0
+        while i < queue_length:
+            next_number = numbers.dequeue()
+            if next_number % value != 0:
+                numbers.enqueue(next_number)
+            i += 1
+        if value > (n ** (1 / 2)):
+            break
+
+    remaining_length = len(numbers)
+    i = 0
+    while i < remaining_length:
+        result.enqueue(numbers.dequeue())
+        i += 1
+
+    return result
+
+
