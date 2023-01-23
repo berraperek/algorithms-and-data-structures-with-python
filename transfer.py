cd54268e03b79b190315525a2642da1f2fa166f6
Berra Perek <73835553+berraperek@users.noreply.github.com>
2023-01-23 17:19:24 +0300

    Update transfer.py

--- transfer.py
+++ transfer.py
@@ -1,3 +1,9 @@
+"""
+Implement a function with signature transfer(S, T) that transfers all elements from stack S onto stack T, so that the element
+that starts at the top of S is the first to be inserted onto T, and the element at the bottom of S ends up at the top of T.
+
+"""
+
 from Stack import Stack
 
 S = Stack()
